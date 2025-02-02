e6b17c6d3bd969b501d00f24b05e563ce8cdc450
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-02-02 19:22:28 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,8 +5,8 @@ int main() {
     scanf("%c",&c);
     if (c>='a'&& c<='z'|| c>='A'&& c<='Z'){
     if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='I'||c=='E'||c=='O'||c=='U')
-    printf("Consonant");
-    else printf("Vowel");
+    printf("Vowel");
+    else printf("Consonant");
         }
     else if (c>='0'&& c<='9'){
         printf("Digit");
